e017a16ef4ca11214cdeec2ce3a8e36c963fada7
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2023-12-09 02:48:39 +0000

    done

--- jsproject/randomcolor.js
+++ jsproject/randomcolor.js
@@ -3,10 +3,11 @@ return(`#${Math.random().toString(16).slice(2,8).padEnd(6,'0')}`);
 }
 let intervalId;
 const startchangingcolor=function(){
-    intervalId=setInterval(changebgcolor, 1000);
-    function changebgcolor(){
+     function changebgcolor(){
     document.body.style.backgroundColor = randomcolor();
     };
+    intervalId=setInterval(changebgcolor, 1000);
+   
 };
 const stopchangingcolor=function(){
     clearInterval(intervalId);
