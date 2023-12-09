96b875275e3deff98aefaa8f0d5d79ae1ad26cbd
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2023-12-09 07:55:05 +0000

    done

--- countdoown/script.js
+++ countdoown/script.js
@@ -1,10 +1,6 @@
 const endDate="10 Dec 2023 10:00 PM";
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
-    // const clock = () => {
-
-// }
-
 function clock() {
     const end = new Date(endDate)
     const now = new Date()
