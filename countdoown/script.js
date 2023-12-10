033598af2ad375f64ac65616a6682362a3e916bd
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2023-12-10 09:42:59 +0000

    done

--- countdoown/script.js
+++ countdoown/script.js
@@ -1,4 +1,4 @@
-const endDate="10 Dec 2023 10:00 PM";
+const endDate="12 Dec 2023 10:00 PM";
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
 function clock() {
